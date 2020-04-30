23db7113f84e91b120e5c0480bc95b1c8cc4b70c
CarlosIsaac1803 <52302183+CarlosIsaac1803@users.noreply.github.com>
2020-04-29 21:31:58 -0500

    Update pedido.js

--- js/pedido.js
+++ js/pedido.js
@@ -31,7 +31,7 @@ document.querySelector("#submit").addEventListener("click", e => {
     }
     resp.classList.remove("fail");*/
     resp.classList.add("send");
-    resp.innerHTML = `Se ha enviado tu reserva, ${cliente}`;
+    resp.innerHTML = `Se ha enviado tu reserva`;
     
     window.open(url);
   });
